8afd803a8861a4f74020870c16f775cc5686230e
Jan Plewa <jan.plewa@web.de>
2023-11-13 16:55:57 +0100

    edited the behaviour so the button now opens a webpage in a new tab referencing the url of the button href element

--- popup.js
+++ popup.js
@@ -1,9 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() { // this waits for the popup.html to fully load
   var checkPageButton = document.getElementById('checkPage');
   checkPageButton.addEventListener('click', function() {
-      chrome.tabs.getSelected(null, function(tab) {
-          d = document;
-          window.open('https://www.google.com/search?q=Zulqadar Idrishi', 'blank');
-      });
+      var url = checkPageButton.getAttribute('href');
+      window.open(url, 'blank');
   }, false);
 }, false);
