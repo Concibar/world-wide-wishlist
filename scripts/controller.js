e68a853b1a4b50fb6cf975c91e9d24756785ee8f
Jan Plewa <jan.plewa@web.de>
2023-12-06 17:49:04 +0100

    Successfully tested Wish saving and reading.

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', async function() { // this waits for the popup.html to fully load
+document.addEventListener('DOMContentLoaded', async function () { // this waits for the popup.html to fully load
 
   // Check the js connections
   popupConnection();
@@ -23,30 +23,42 @@ document.addEventListener('DOMContentLoaded', async function() { // this waits f
 
   // Save Button Submit Form
   const saveButton = document.getElementById('save-wish');
-  saveButton.addEventListener('click', function() {
-    chrome.storage.local.set({'test': document.getElementById('wish-name').value});
-    console.log("I saved the thing");
-    console.log(document.getElementById('wish-name').value);
-  });
+  saveButton.addEventListener('click', () => {
+    saveTesting(200);
+
+    // let asdf = getFormData();
+    // let wish = new Wish(asdf);
+    // console.log(wish);
+    // wish.save();
+
+    // chrome.storage.local.set({'test': document.getElementById('wish-name').value});
+    // console.log("I saved the thing");
+    // console.log(document.getElementById('wish-name').value);
+  }, false);
 
   // Read Button (for testing only) Will become Exit later
   const readButton = document.getElementById('escape-button');
   readButton.addEventListener('click', () => {
-    chrome.storage.local.get('test', function(result) {
-      console.log(result.test);
-    })
+    console.log("reading wishes from storage:")
+    chrome.storage.local.get('wishes', (result) => {
+      console.log(result.wishes);
+    });
+
+    // chrome.storage.local.get('0', function (result) {
+    //   console.log(result);
+    // })
   }, false);
 
   // Go-To-Wishlists
   const WishlistsButton = document.getElementById('go-to-wishlists-button');
   WishlistsButton.addEventListener('click', () => {
-    chrome.tabs.create({url: chrome.runtime.getURL('mywishlist.html')});
+    chrome.tabs.create({ url: chrome.runtime.getURL('mywishlist.html') });
   }, false);
 
   // Settings
   const settingsButton = document.getElementById('settings-button');
   settingsButton.addEventListener('click', () => {
-    chrome.tabs.create({url: chrome.runtime.getURL('settings.html')});
+    chrome.tabs.create({ url: chrome.runtime.getURL('settings.html') });
   }, false);
 
 }, false);
--- scripts/wish.js
+++ scripts/wish.js
@@ -4,44 +4,68 @@ function wishConnection() {
 
 class Wish {
   constructor({
-    id,
-    name,
-    url,
-    imageArray = ["images/whoopsie.png"],
-    price_in_cents,
-    currency = "default_currency",
-    quantity = 1,
-    note,
-    date}) {
-    this.id = id;
-    this.name = name;
-    this.url = url;
-    this.image = imageArray;
-    this.price_in_cents = price_in_cents;
-    this.currency = currency;
-    this.quantity = quantity;
-    this.note = note;
-    this.Date = date;
-    console.log(`Wish Object created with name: ${this.wishName}`);
+      id,
+      wishlistId,
+      name,
+      url,
+      image = "images/whoopsie.png",
+      priceInCents,
+      currency,
+      quantity = 1,
+      note,
+      date = "a hot minute ago" //Put the Now Time here somehow
+    }) {
+      this.id = id;
+      this.wishlistId = wishlistId;
+      this.name = name;
+      this.url = url;
+      this.image = image;
+      this.priceInCents = priceInCents;
+      this.currency = currency;
+      this.quantity = quantity;
+      this.note = note;
+      this.date = date;
+    };
+
+  async save() {
+    // console.log("save function connected")
+    let wishData = {
+      "id": this.id,
+      "wishlistId": this.wishlistId,
+      "name": this.name,
+      "url": this.url,
+      "image": this.image,
+      "priceInCents": this.priceInCents,
+      "currency": this.currency,
+      "quantity": this.quantity,
+      "note": this.note,
+      "date": this.date
+    };
+    // console.log("Type of wishData: " + typeof wishData.wishes);
+    // console.log(wishData);
+
+    let result = await chrome.storage.local.get(['wishes']);
+    let wishes = result.wishes;
+    // console.log("Result of reading aka result:" + result);
+    // console.log("Type of result: " + typeof result);
+    // console.log("Type of wishes: " + typeof wishes);
+
+    wishes.push(wishData);
+    // console.log("wishes after pushing");
+    // console.log(wishes);
+    // console.log(wishes[3]);
+
+    chrome.storage.local.set({'wishes': wishes});
   };
 
-  // Todo: Create item method (based on data handed over from controller.js)
-  wishCreate(name) {
-    chrome.storage.local.set({
-      'name': name
-    }), () => {
-      console.log("wish was saved boys and girls!")
-    };
-  };
+  class
 
   // Todo: Read a wish method
-  wishRead(key) {
-    chrome.storage.local.get(key, )
+  read() {
+
   }
 
   // Todo: Destroy a wish method
 
   // Todo: update a wish method
-
-  // safe, find, destroy, update,
 };
